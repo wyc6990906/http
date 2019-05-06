6a5e51f9416b6b0ecc2f4b7a06a29c2f2ff54704
Yichuan Wang <wyc6990906@hotmail.com>
2019-05-06 02:42:18 +0000

    6thd step

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require("https");
 
 console.log("I did it!");
+
+var options = {
+  host: "www.example.org",
+  path: "/"
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log("In response handler callback!");
+
+  console.log("Response: ", response);
+};
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
